75cdff67b645eb032c5e9adf0490240f17d22170
flixabua <ge38xes@mytum.de>
2023-04-14 19:32:13 +0200

    movement by force instead of direct

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,14 +2,31 @@ using System;
 using UnityEngine;
 using System.IO.Ports;
 using System.Numerics;
+using TMPro;
 using Vector3 = UnityEngine.Vector3;
 
 public class InputManager : MonoBehaviour
 {
     private float ControllerValue;
     public int AmountToMove;
+    public GameObject air;
+    public GameObject depth;
     private Rigidbody _rigidbody;
     private float lastData = 0f;
+    private TMP_Text AirText;
+    private TMP_Text DepthText;
+    private TMP_Text JacketText;
+    
+    
+    private float currentAir = 0f;
+    private float currentDepth = 10f;
+    private float lastDepth = 10f;
+    private float currentJacketAir = 4f;
+    private float additionalVolume = 10f;
+    private float DiverVolume = 65f;
+    private float Mass = 80f;
+    private float force = 0f;
+    
 
     private SerialPort serial = new SerialPort("COM11", 9600);
     // Start is called before the first frame update
@@ -17,6 +34,10 @@ public class InputManager : MonoBehaviour
     {
         
         _rigidbody = GetComponent<Rigidbody>();
+        AirText = air.GetComponent<TMP_Text>();
+        DepthText = depth.GetComponent<TMP_Text>();
+        AirText.SetText("Current air in Lung: {0}", currentAir);
+        DepthText.SetText("Current Depth: {0}", currentDepth);
     }
 
     // Update is called once per frame
@@ -27,25 +48,35 @@ public class InputManager : MonoBehaviour
             serial.Open();
             serial.ReadTimeout = 10;
         }
-
+    
         float data;
         try
         {
-            data = float.Parse(serial.ReadLine()) / 1000;
+            data = float.Parse(serial.ReadLine()) / 1000000;
             lastData = data;
         }
         catch (TimeoutException)
         {
             data = lastData;
         }
+    
+        currentAir += data;
+        CalculateVolume();
+        currentDepth = -_rigidbody.position.y + 10;
+        AirText.SetText("Current air in Lung/Jacket: {0}/{1}", currentAir, currentJacketAir);
+        CalculateForce();
+        DepthText.SetText("Current Depth: {0} force: {1}", currentDepth, force);
+        
         Debug.Log(data);
-        MoveObject(data);
-        
-        
+        // MoveObject(data);
+        MoveObjectbyForce(force);
+
+
     }
 
     void MoveObject(float value)
     {
+        currentDepth = -_rigidbody.position.y + 10;
         Debug.Log(value);
         if (value > 0)
         {
@@ -58,5 +89,30 @@ public class InputManager : MonoBehaviour
             _rigidbody.velocity = new Vector3(0.0f, value, 0.0f) * AmountToMove; 
             // transform.position += Vector3.up*value;
         }
+    }
+
+    void MoveObjectbyForce(float force)
+    {
+        Vector3 appiedForce = new Vector3(0f, force, 0f);
+        
+        _rigidbody.AddForce(appiedForce);
+    }
+
+    void CalculateForce()
+    {
+        float fAuf = (currentAir + currentJacketAir + additionalVolume + DiverVolume);
+        float fAb = Mass;
+        force = (fAuf - fAb);
+    }
+
+    void CalculateVolume()
+    {
+        float new_pressure = 1f + (currentDepth / 10f);
+        float old_pressure = 1f + (lastDepth / 10f);
+        currentAir = (old_pressure * currentAir) / new_pressure;
+        currentJacketAir = (old_pressure * currentJacketAir) / new_pressure;
+        additionalVolume = (old_pressure * additionalVolume) / new_pressure;
+        lastDepth = currentDepth;
+
     }
 }
